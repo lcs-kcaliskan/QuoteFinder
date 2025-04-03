a43de2e40836f3e238af4b7ecd51c0a9edb879c8
Kaan Caliskan <kcaliskan@lcs.on.ca>
2025-04-03 08:45:32 -0400

    Initial Commit

---
+++ QuoteFinder/QuoteFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteFinderApp.swift
+//  QuoteFinder
+//
+//  Created by Ahmet Kaan Caliskan on 2025-04-03.
+//
+
+import SwiftUI
+
+@main
+struct QuoteFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
